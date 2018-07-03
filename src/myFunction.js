96e5e715b80b6891910178057fe49d286bd77e04
yuhanyuhan <yuhanlian@gmail.com>
2018-07-03 11:32:18 +0800

    final submission + test passed

--- src/myFunction.js
+++ src/myFunction.js
@@ -1,5 +1,15 @@
-const myFunction = () => {
-  return "something";
+const myFunction = x => {
+    var total = ((Object.keys(x).reduce((accumulator, b) => { 
+      console.log(b, x[b]);
+      return (accumulator + x[b])
+    }, 0)))
+    var totalPeople = Object.keys(x).length
+    var avg = (total/totalPeople)
+    
+    for (var key in x) {
+      x[key] = Number((x[key] - avg).toFixed(2))
+    }
+  return x
 };
 
 module.exports = myFunction;
--- test/myFunction.test.js
+++ test/myFunction.test.js
@@ -1,5 +1,5 @@
 const myFunction = require("../src/myFunction");
 
 it("should be replaced with a descriptive message", () => {
-  expect(myFunction()).toEqual("something");
+  expect(myFunction({A: 20, B: 15, C: 10})).toEqual({A: 5, B: 0, C: -5});
 });
